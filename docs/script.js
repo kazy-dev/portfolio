3cde7097c92d4b8602004537305f9caf02f742e7
Yoshikazu <yoshikazu@Y-MacBook-Pro.local>
2019-03-11 16:20:55 +0900

    Aboutタップでコンテンツまでスクロールするように修正。

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,15 @@
 $(function(){
     $('#age').text('年齢 ' + getAge(1989,5,15) + '歳');
 
+    var p = $('.top_contents_list').find('#list_about');
+    p.click(function() {
+        var position = $('.about').offset().top;
+        console.log(position);
+        $("html,body").animate({
+            scrollTop:position
+        }, 500);
+    })
+
 function getAge(year, month, day) {
   var birthday  = new Date(year, month-1, day);
   var today = new Date();
